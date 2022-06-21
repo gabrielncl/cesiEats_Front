6e966b8c0293cad2dbe1599b556722dd4d24a970
DylanS <dylan.savaresse@viacesi.fr>
2022-06-21 15:02:42 +0200

    testing

--- user/src/router/index.ts
+++ user/src/router/index.ts
@@ -11,6 +11,11 @@ const routes: Array<RouteConfig> = [
     component: HomeView
   },
   {
+    path: '/',
+    name: 'cart',
+    component: HomeView
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
