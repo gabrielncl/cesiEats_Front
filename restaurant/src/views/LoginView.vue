c28fd9455ac587ce25d35400b9987a53e473d87e
DylanS <dylan.savaresse@viacesi.fr>
2022-06-22 14:49:54 +0200

    Login and register mockup for restaurant

--- restaurant/src/views/LoginView.vue
+++ restaurant/src/views/LoginView.vue
@@ -1,58 +1,40 @@
 
 <template>
-  <div class="login">
-    <form>
-      <!-- Email input -->
-      <div class="form-outline mb-4 ">
-        <input type="email" id="form2Example1" class="form-control" />
-        <label class="form-label" for="form2Example1">Email address</label>
-      </div>
+    <div class="container">
+        <div class="login">
+            <form>
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="form2Example1" class="input form-control mb-1" />
+                    <label class="form-label" for="form2Example1">Email address</label>
+                </div>
 
-      <!-- Password input -->
-      <div class="form-outline mb-4">
-        <input type="password" id="form2Example2" class="form-control" />
-        <label class="form-label" for="form2Example2">Password</label>
-      </div>
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="form2Example2" class="input form-control mb-1" />
+                    <label class="form-label" for="form2Example2">Password</label>
+                </div>
 
-      <!-- 2 column grid layout for inline styling -->
-      <div class="row mb-4">
-        <div class="col d-flex justify-content-center">
-          <!-- Checkbox -->
-          <div class="form-check">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="form2Example31"
-              checked
-            />
-            <label class="form-check-label" for="form2Example31">
-              Remember me
-            </label>
-          </div>
+                <!-- Submit button -->
+                <button type="button" class="btn btn-primary btn-block mb-4">
+                    Sign in
+                </button>
+
+                <!-- Register buttons -->
+                <div class="text-center">
+                    <p>Not a member? <a href="#!">Register</a></p>
+                </div>
+            </form>
         </div>
-
-        <div class="col">
-          <!-- Simple link -->
-          <a href="#!">Forgot password?</a>
-        </div>
-      </div>
-
-      <!-- Submit button -->
-      <button type="button" class="btn btn-danger btn-block mb-4">
-        Sign in
-      </button>
-
-      <!-- Register buttons -->
-      <div class="text-center">
-        <p>Not a member? <a href="#!">Register</a></p>
-      </div>
-    </form>
-  </div>
+    </div>
 </template>
 
 <style>
-
+    .input {
+        max-width: 40%;
+        margin-left: auto;
+        margin-right: auto ;
+    }
 </style>
 
 <script lang="ts">
