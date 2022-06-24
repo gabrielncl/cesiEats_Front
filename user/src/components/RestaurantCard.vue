e4b5c3f83d545f71822e9523c740c59c79f388ab
DylanS <dylan.savaresse@viacesi.fr>
2022-06-24 10:20:00 +0200

    starting

---
+++ user/src/components/RestaurantCard.vue
@@ -0,0 +1,15 @@
+<template>
+<div>CEci est une carte</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class RestaurantCard extends Vue {
+
+}
+</script>
+
+<style>
+</style>
--- user/src/views/HomeView.vue
+++ user/src/views/HomeView.vue
@@ -7,11 +7,11 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import RestaurantCard from '@/components/RestaurantCard.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    RestaurantCard,
   },
 })
 export default class HomeView extends Vue {}
