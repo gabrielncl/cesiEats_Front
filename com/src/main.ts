92c35803c7315585883addcf723f02eecbd1d7ba
Eva <evamadani@hotmail.fr>
2022-06-27 09:52:36 +0200

    add vuetify

--- com/src/main.ts
+++ com/src/main.ts
@@ -6,6 +6,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -15,5 +16,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
