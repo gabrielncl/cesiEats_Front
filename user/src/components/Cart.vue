eba21ecb2a4b9b72bf5c4a0a869cb47720d16a32
Gabriel <gabriel.niculae@outlook.fr>
2022-06-29 09:31:57 +0200

    Working on user

--- user/src/components/Cart.vue
+++ user/src/components/Cart.vue
@@ -1,31 +1,44 @@
 <template>
-  <tr>
-    <td>
-      <v-list-item key="1" @click="">
-        <v-list-item-avatar>
-          <v-img :src="require('../assets/logo.png')"></v-img>
-        </v-list-item-avatar>
+	<tr>
+		<td>
+			<v-list-item key="1" @click="">
+				<v-list-item-avatar>
+					<v-img :src="require('../assets/logo.png')"></v-img>
+				</v-list-item-avatar>
 
-        <v-list-item-content>
-          <v-list-item-title>Nom de l'article</v-list-item-title>
-          <v-list-item-subtitle>Nom du restaurant</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </td>
-    <td>40.00 euros</td>
-    <td>
-      <v-text-field
-        class="pt-10"
-        label="Outlined"
-        style="width: 80px"
-        single-line
-        outlined
-        value="2"
-        type="number"
-      ></v-text-field>
-    </td>
-    <td>80.00 euros</td>
-    <td><a>X</a></td>
-  </tr>
+				<v-list-item-content>
+					<v-list-item-title>{{ name }}</v-list-item-title>
+					<v-list-item-subtitle>Nom du restaurant</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
+		</td>
+		<td>{{ price }} € </td>
+		<td>
+			<v-text-field
+				class="pt-10"
+				label="Outlined"
+				style="width: 80px"
+				single-line
+				outlined
+				value="2"
+				type="number"
+			></v-text-field>
+		</td>
+		<td>80.00 euros</td>
+		<td><a>X</a></td>
+	</tr>
 </template>
-<script></script>
+<script>
+export default {
+	props: {
+		name: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+	},
+};
+</script>
--- user/src/views/ArticleView.vue
+++ user/src/views/ArticleView.vue
@@ -1,19 +1,17 @@
 <template>
 	<div>
+		<Navbar />
 		<v-container>
 			<v-app>
-				<Navbar />
+                <h1 class="mb-2 text-center">Commander Articles & Menus</h1>
 				<v-row no-gutters>
-					<v-col :cols="12">
-						<v-card-text class="" tile outlined>
-							<div class="text-center">
-								<h1 class="mb-2">Commander</h1>
-							</div>
-							<div v-for="article in articles">
-								<Article v-bind:name="article.name" v-bind:id="article._id" v-bind:price="article.price"></Article>
-							</div>
-						</v-card-text>
-					</v-col>
+					<div v-for="article in articles">
+						<Article
+							v-bind:name="article.name"
+							v-bind:id="article._id"
+							v-bind:price="article.price"
+						></Article>
+					</div>
 				</v-row>
 				<Footer />
 			</v-app>
@@ -42,7 +40,7 @@ export default {
 	mounted() {
 		const url = window.location.href;
 		const id = url.split("/")[url.split("/").length - 2];
-        console.log(id);
+		console.log(id);
 		axios.get("http://api.cesieats.loc/shop/" + id).then((response) => {
 			this.articles = response.data.articles;
 			console.log(this.articles);
